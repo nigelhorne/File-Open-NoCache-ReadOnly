e0ca86fe7c0a8a9d0b54f6b91db23e4e98467778
Nigel Horne <njh@bandsman.co.uk>
2025-06-02 19:57:47 -0400

    Release 0.06

--- lib/File/Open/NoCache/ReadOnly.pm
+++ lib/File/Open/NoCache/ReadOnly.pm
@@ -22,11 +22,11 @@ File::Open::NoCache::ReadOnly - Open a file and flush from memory on closing
 
 =head1 VERSION
 
-Version 0.05
+Version 0.06
 
 =cut
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 =head1 DESCRIPTION
 
